5d3eea04e3419980ad8a4a593fafc9cbe6d1df51
nathalie <nathalie.metreau@hotmail.fr>
2023-09-12 00:56:41 +0200

    correct the binary tree is perfect function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,22 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - function  that checks if a node is a leaf
+ *
+ * @node: pointer on parent's adress
+ *
+ * Return: 1 for true or 0 for false
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
+}
 
 /**
  * binary_tree_height - measures the height of a binary tree
@@ -41,9 +59,22 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int left = 0;
+	int right = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	return (binary_tree_height(tree->left)
-	 == binary_tree_height(tree->right));
+	if (binary_tree_is_leaf(tree))
+	return (1);
+
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left != right)
+	return (0);
+
+	return (binary_tree_is_perfect(tree->left) &&
+	binary_tree_is_perfect(tree->right));
+
 }
