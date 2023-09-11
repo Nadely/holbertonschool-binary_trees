eb2f9167ee969f4aa23b09d7f902a550028417e7
Nadely <6890@holbertonstudents.com>
2023-09-11 14:32:26 +0200

    correction in binary_tree_insert left and right for corrected link nodes

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,16 +20,15 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->right == NULL)
-		parent->right = new_node;
-
-	else
+	if (parent->right != NULL)
 	{
 		new_node->right = parent->right;
+		new_node->parent = parent->right->parent;
 		parent->right->parent = new_node;
 	}
+
 	parent->right = new_node;
-	new_node->parent = parent;
+	new_node->parent = parent->right->parent;
 
 	return (new_node);
 }
