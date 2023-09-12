2f2af34699338379b02c1001ed77905ad3436868
Nadely <6890@holbertonstudents.com>
2023-09-12 13:57:15 +0200

    ccorrection binary tree is full whit add binary tree leaf in function and correction conditions

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,6 +1,25 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - function  that checks if a node is a leaf
+ *
+ * @node: pointer on parent's adress
+ *
+ * Return: 1 for true or 0 for false
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+
+	return (0);
+}
+
+/**
  * binary_tree_is_full - function that checks if a binary tree is full
  *
  * @tree: pointer on parent's adress
@@ -9,22 +28,19 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int left, right;
+
 	if (tree == NULL)
-		return (1);
-
-	if ((tree->left != NULL && tree->right != NULL) &&
-		binary_tree_is_full(tree->left) &&
-		binary_tree_is_full(tree->right))
-		return (1);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	if (tree->left == NULL && tree->right != NULL)
 		return (0);
 
-	if (tree->right == NULL && tree->left != NULL)
+	if (binary_tree_is_leaf(tree))
 		return (1);
-		
-	return (0);
+
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+
+	if (left == 0 || right == 0)
+		return (0);
+
+	return (1);
 }
