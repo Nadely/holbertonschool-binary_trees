0f79900a7fad4f2f86351b53d7068ef731e46a0f
Nadely <6890@holbertonstudents.com>
2023-09-12 15:15:05 +0200

    added binary tree  sivling and uncle function

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - function that finds the uncle of a node
+ *
+ * @node: pointer on parent's adress
+ *
+ * Return: the uncle of a node
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grand_parent = node->parent->parent;
+
+	if (node == NULL || node->parent == NULL||node->parent->parent == NULL)
+		return (NULL);
+
+	if (grand_parent->left == node->parent)
+		return (grand_parent->right);
+	else
+		return (grand_parent->left);
+}
