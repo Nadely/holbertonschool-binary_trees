48cadcdc3ebe3ce5318b8072a0d04754323af1b8
Nadely <6890@holbertonstudents.com>
2023-09-12 13:27:45 +0200

    add one condition in binary_tree_is_full fonction for the case where right is null and left not

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -23,5 +23,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right != NULL)
 		return (0);
 
+	if (tree->right == NULL && tree->left != NULL)
+		return (1);
+		
 	return (0);
 }
