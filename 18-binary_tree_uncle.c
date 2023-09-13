fa77e2f3ddd16f90f562fefb1ac493c71ccf6e5b
Nadely <6890@holbertonstudents.com>
2023-09-13 10:35:35 +0200

    modify the binary tree sibling and binary tree uncle functions to remove the last condition and restore the condition if parent or grandparent left and right are null return null

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -15,10 +15,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
+	if (grand_parent->left == NULL || grand_parent->right == NULL)
+		return (NULL);
+
 	if (grand_parent->left == node->parent)
 		return (grand_parent->right);
 	else if (grand_parent->right == node)
 		return (grand_parent->left);
-	else
-		return (NULL);
 }
